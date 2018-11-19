524c56336e30411fc9747b53d000f239e696d4e2
Giorgio Guttilla <gguttilla@wustl.edu>
2018-11-19 14:09:33 -0600

    log out functionality

--- src/routes/MainPage/MainPage.js
+++ src/routes/MainPage/MainPage.js
@@ -3,23 +3,40 @@ import WorldMap from '../../components/Map/Map';
 import { withRouter, Redirect } from 'react-router-dom';
 import * as firebase from 'firebase';
 
-const Menu = () => {
-    return(
-        null
-        //
-    );
-};
+
+
+
 
 class MainPage extends Component {
-    
+    constructor(props){
+        super(props);
+        this.state = {
+            redirect: ''
+        };
+    }
+
+
+    logOut = () => {
+        var thisMP = this;
+
+        firebase.auth().signOut().then(
+            thisMP.setState({redirect: '/'})
+        );
+    }
+
     render(){
 
         //redirects back to login if nobody is signed in
         if(!firebase.auth().currentUser){
             return <Redirect to='/'/>
+        } else if (this.state.redirect === '/'){
+            return <Redirect to='/'/>
         } else {
             return(
                 <div> 
+                    <button onClick={this.logOut}>
+                        Log Out
+                    </button>
                     <WorldMap />
                 </div>
             );
